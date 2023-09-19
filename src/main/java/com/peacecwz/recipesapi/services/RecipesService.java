292721b7a7beaf4e660665c036c63d9e4aa9d3a9
Baris Ceviz <baris@ceviz.dev>
2023-09-19 03:06:48 +0200

    feat: implement get recipes function

--- src/main/java/com/peacecwz/recipesapi/services/RecipesService.java
+++ src/main/java/com/peacecwz/recipesapi/services/RecipesService.java
@@ -9,15 +9,20 @@ import com.peacecwz.recipesapi.data.TagEntity;
 import com.peacecwz.recipesapi.dtos.RecipeDto;
 import com.peacecwz.recipesapi.repositories.IngredientRepository;
 import com.peacecwz.recipesapi.repositories.RecipeRepository;
-import com.peacecwz.recipesapi.repositories.RecipeSpecifications;
 import com.peacecwz.recipesapi.repositories.TagRepository;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.Subquery;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -33,10 +38,59 @@ public class RecipesService {
     }
 
     public GetRecipesResponse getRecipes(GetRecipesRequest request) {
-        return null;
+        Pageable pageable = PageRequest.of(request.getPage() - 1, request.getSize());
+
+        Specification<RecipeEntity> spec = (root, query, cb) -> {
+            List<Predicate> predicates = new ArrayList<>();
+
+            if (request.getTags() != null) {
+                predicates.add(root.join("tags").get("name").in(Arrays.asList(request.getTags())));
+            }
+
+            if (request.getIncludeIngredients() != null) {
+                predicates.add(root.join("ingredients").get("name").in(Arrays.asList(request.getIncludeIngredients())));
+            }
+
+            if (request.getExcludeIngredients() != null) {
+                Subquery<RecipeEntity> subquery = query.subquery(RecipeEntity.class);
+                Root<RecipeEntity> subqueryRoot = subquery.from(RecipeEntity.class);
+                subquery.select(subqueryRoot)
+                        .where(subqueryRoot.join("ingredients").get("name").in(Arrays.asList(request.getExcludeIngredients())));
+                predicates.add(cb.not(root.in(subquery)));
+            }
+
+            if (request.getServings() != null) {
+                predicates.add(cb.equal(root.get("servings"), request.getServings()));
+            }
+
+            if (request.getTerm() != null) {
+                Predicate namePredicate = cb.like(cb.lower(root.get("name")), "%" + request.getTerm().toLowerCase() + "%");
+                Predicate descriptionPredicate = cb.like(cb.lower(root.get("description")), "%" + request.getTerm().toLowerCase() + "%");
+                Predicate instructionsPredicate = cb.like(cb.lower(root.get("instructions")), "%" + request.getTerm().toLowerCase() + "%");
+
+                Predicate orPredicate = cb.or(namePredicate, descriptionPredicate, instructionsPredicate);
+                predicates.add(orPredicate);
+            }
+
+            return cb.and(predicates.toArray(new Predicate[0]));
+        };
+
+        Page<RecipeEntity> recipePage = recipeRepository.findAll(spec, pageable);
+
+        // Assuming that toDto is a method to transform entity to DTO
+        List<RecipeDto> recipeDtos = recipePage.getContent().stream()
+                .map(r -> RecipeDto.buildDto(r, r.getIngredients(), r.getTags()))
+                .collect(Collectors.toList());
+
+        return GetRecipesResponse.builder()
+                .data(recipeDtos)
+                .page(request.getPage())
+                .size(request.getSize())
+                .total(recipePage.getTotalPages())
+                .build();
     }
 
-    public boolean createRecipe(CreateRecipeRequest request) {
+    public void createRecipe(CreateRecipeRequest request) {
         Set<TagEntity> tags = request.getTags().stream()
                 .map(tagName -> tagRepository.findByName(tagName).orElseGet(() -> tagRepository.save(TagEntity.builder().name(tagName).build())))
                 .collect(Collectors.toSet());
@@ -58,6 +112,5 @@ public class RecipesService {
 
         recipeRepository.save(recipe);
 
-        return true;
     }
 }
